61397897317b4fa6753d8a8c0f4b793fb2f88676
umarfarooq95 <mohamedumar931@gmail.com>
2020-06-11 08:34:31 +0530

    added dyanmic-key access and timeout js files

---
+++ exercises/timeout.js
@@ -0,0 +1,16 @@
+function sayHi() {
+    console.log('Hello');
+}
+
+const sayBye = () => {
+    console.log("bye")
+}
+
+//                      Exercises
+
+/**
+ *  1.  Call or Invoke sayHi function after 3 seconds
+ * 
+ *  2.  Call or Invoke sayBye function after 10 seconds
+ */
+
