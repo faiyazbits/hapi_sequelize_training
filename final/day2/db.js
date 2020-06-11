6c3b772f167bc93394a67d0a78a10778bbc8a188
faiyazbits <m.faiyaz123@gmail.com>
2020-06-11 15:46:26 +0530

    day2 code

---
+++ final/day2/db.js
@@ -0,0 +1,18 @@
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('hapi_training', 'postgres', 'faiyaz', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+}).catch((err) => {
+  console.log(err);
+});
